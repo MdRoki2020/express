4d8d086b353c45549de538b30ca175b4af28f369
Md.Roki <73433502+MdRoki2020@users.noreply.github.com>
2022-03-29 20:35:06 +0600

    set_up_environment_variables

---
+++ set_up_environment_variables/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express=require('express')
+const app=express()
+const PORT=process.env.PORT || 3000;
+
+
+app.get('/',(req,res)=>{
+    res.send('awwww')
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running at http://localhost:${PORT}`)
+})
