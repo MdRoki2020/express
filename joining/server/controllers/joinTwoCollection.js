7014f5f4cf06763cfe52a573a3c0f7390c763a4f
MdRoki2020 <mroki815@gmail.com>
2022-08-27 22:57:39 +0600

    working on joining two collection

--- joining/server/controllers/joinTwoCollection.js
+++ joining/server/controllers/joinTwoCollection.js
@@ -1,21 +1,19 @@
 const PostCommentModel=require('../models/PostCommentModel');
 
-exports.joinTwoCollection= PostCommentModel.aggregate([{
-    $lookup: {
-        from: "posts", // collection to join
-        localField: "PostId",//field from the input documents
-        foreignField: "_id",//field from the documents of the "from" collection
-        as: "posts"// output array field
+exports.joinTwoCollection=async (err,data)=>{
+    try{
+        data=await PostCommentModel.aggregate([
+
+            {
+                $lookup:{from :"posts",localField:"PostId",foreignField:"_id",as:"posts" }
+            
+            }
+            
+            ])
+    }catch(err){
+        return {status: "fail", data: err.toString()}
     }
-}],function (err, data) {
-return res.json(data);
-//handle error case also
-
-});
-
-
-
-
+}
 
 // exports.joinTwoCollection=async (err,data)=>{
 //     try{
